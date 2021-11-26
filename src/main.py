489238b9686a9ccf5989132e8d7693627b5972ed
Jorge Uliana <ulianamjjorge@gmail.com>
2021-11-26 12:39:33 -0300

    Applied the suggested changes...

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ def show_video(video):
             video_frames.append(frame)
         else:
             break
-        if cv2.waitKey(1000 // framerate) == ord('k'):
+        if cv2.waitKey(100 // framerate) == ord('k'):
             break
         
 
@@ -50,12 +50,14 @@ def perform_bg_subtraction(video_frames):
     mog2 = cv2.createBackgroundSubtractorMOG2(N_frames)
 
     # Applying the images into the MOG2
+    mogged_frames = []
     for frame in video_frames:
         img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        mog2.apply(img) # This works like an "average"
+        mog_iter = mog2.apply(img) # This works like an "average"
+        mogged_frames.append(cv2.cvtColor(mog_iter, cv2.COLOR_BGR2RGB))
 
     # Retuning the obtained background:
-    return cv2.cvtColor(mog2.getBackgroundImage(), cv2.COLOR_BGR2RGB)
+    return cv2.cvtColor(mog2.getBackgroundImage(), cv2.COLOR_BGR2RGB), mogged_frames
 
 def play_video(video_frames, framerate, res_func):
     for frame in video_frames:
@@ -72,9 +74,9 @@ def treat_image(frame):
     _, thresh = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY_INV)
 
     # Performing the erosion and the dilatation:
-    kernel_erosion = np.ones((5, 5), np.uint8) # The kernels were chosen by "guess and trial"
+    kernel_erosion = np.ones((9, 9), np.uint8) # The kernels were chosen by "guess and trial"
     eroded = cv2.erode(thresh, kernel_erosion)
-    kernel_dilatation = np.ones((1, 1), np.uint8)
+    kernel_dilatation = np.ones((9, 9), np.uint8)
     return cv2.dilate(eroded, kernel_dilatation)
 
 
@@ -83,53 +85,35 @@ def main():
     video = load_video_from_argv()
     video_frames = show_video(video)
 
-    # Showing the obtained background:
-    background = perform_bg_subtraction(video_frames)
-    cv2.imshow('Obtained background', background)
-    cv2.waitKey(0)
+    # Using the MOG2 algorithm to obtain the filtered image:
+    background, mogged_frames = perform_bg_subtraction(video_frames)
 
-    # Showing each bg subtracted frame:
+    # Showing each 'mogged' frame:
     play_video(
-        video_frames,
+        mogged_frames,
         framerate = 120, # really fast (but no so fast)
-        res_func = lambda frame : frame - background
+        res_func = lambda frame : frame
     )
-
-    # Performing the specified operations in the image
-    dilated_bg = treat_image(background)
 
     # Showing each treated frame:
     play_video(
-        video_frames,
+        mogged_frames,
         framerate = 120, # really fast (but no so fast)
-        res_func = lambda frame : treat_image(frame) - dilated_bg
+        res_func = lambda frame : treat_image(frame)
     )
-
-    # These color are defined in order to be used later in the "filtering"
-    clear_gray = np.array([128, 128, 128])
-    white = np.array([255, 255, 255])
     
     # The framerate is low so we can see each frame slowly
     framerate = 30
-    for frame in video_frames:
-        # We treat each frame, as we treated the background, so they are "compatible":
-        dilated_frame = treat_image(frame)
-
-        # The subtraction will remove any intersection (the background will be removed):
-        result = dilated_frame - dilated_bg
-
-        # Observing the "result" image, I noticed that the white-ish colors are usually
-        # present where humans/dogs can be seen. So I filtered it...
-        mask = cv2.inRange(result, clear_gray, white) 
-        filtered_res = cv2.bitwise_and(result, result, mask=mask) # Only white-ish colors are present now
+    for mogged_frame, frame in zip(mogged_frames, video_frames):
+        # We treat each mogged frame:
+        result = treat_image(mogged_frame)
 
         # I converted it to grayscale, so the findContours function will work properly
-        filtered_gry = cv2.cvtColor(filtered_res, cv2.COLOR_BGR2GRAY).astype(np.uint8)
-        blurred_res = cv2.blur(filtered_gry, (7, 7)) # I blurred it so the human presence area is "spread" (just a little)
-        outlines, _ = cv2.findContours(blurred_res, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        filtered_gry = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY).astype(np.uint8)
+        outlines, _ = cv2.findContours(filtered_gry, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for c in outlines:
-            if cv2.contourArea(c) <= 5000:
+            if cv2.contourArea(c) <= 2500 or cv2.contourArea(c) >= 0.8 * frame.shape[0] * frame.shape[1]:
                 # I defined the minimum contour area as 5000, so no noise can be influence the bounding boxes 
                 continue
 
